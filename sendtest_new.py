dc9fb19ba8d2fd0c60e671fc4456003d9e23105a
Marek Wiewiorski <marek@mwicat.com>
2012-04-17 12:49:29 +0200

    add test exercising multiple clients in one test case

--- sendtest_new.py
+++ sendtest_new.py
@@ -1,22 +1,24 @@
+import time
+
 from gui import softkeys
-from sccptest import *
+from sccptest.sccptest import *
+
 from sccp.sccpmessagetype import SCCPMessageType
 from sccp.sccpcallstate import SCCPCallState
 from sccp.sccpstarttone import SCCPStartTone
-import time
 
-@client(device='SEP001120AABBCC', serverAddress=['127.0.0.1', 2000])
-@client(device='SEP001120AABBAA', serverAddress=['127.0.0.1', 2000])
-def testClient(client, client2):
-    NUMBER_TO_CALL = '1102'
+
+@client(device='SEP000000000000', serverAddress=('127.0.0.1', 2000), bindAddress=('127.0.0.2', 0))
+@client(device='SEP000000000001', serverAddress=('127.0.0.1', 2000), bindAddress=('127.0.0.3', 0))
+def testCallOperator(client, operator):
+    OPERATOR_NUMBER = '1102'
     expectSoftkey(client, softkeys.SKINNY_LBL_NEWCALL)
     pushSoftKey(client, softkeys.SKINNY_LBL_NEWCALL)
     call = getCallInfo(expectCallState(client, SCCPCallState.SCCP_CHANNELSTATE_OFFHOOK))
     expectTone(client, SCCPStartTone.SCCP_TONE_INSIDE, call=call)
-    dial(client, NUMBER_TO_CALL)
+    dial(client, OPERATOR_NUMBER)
     expectTone(client, SCCPStartTone.SCCP_TONE_INSIDE, call=call)
     expectTone(client, SCCPStartTone.SCCP_TONE_ALERTING, call=call)
-    expectSoftkey(client2, softkeys.SKINNY_LBL_ANSWER)
-    pushSoftKey(client2, softkeys.SKINNY_LBL_ANSWER)
+    expectSoftkey(operator, softkeys.SKINNY_LBL_ANSWER)
+    pushSoftKey(operator, softkeys.SKINNY_LBL_ANSWER)
     time.sleep(1)
-
